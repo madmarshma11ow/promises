b715102da9863ce22ca5806a4002a8d83a0273dc
madmarshma11ow <sashagornostaev35@gmail.com>
2026-04-10 18:19:51 +0300

    init

--- index1.js
+++
@@ -1,7 +0,0 @@
-function delayedAdd(a, b, delay, callback) {
-  setTimeout(() => callback(null, a + b), delay);
-}
-delayedAdd(2, 3, 1000, (err, sum) => {
-  if (err) return console.error(err);
-  console.log('sum =', sum); // 5
-});
