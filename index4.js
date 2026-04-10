8144c9b2751b8b99f8c13aae54d0cd7a585a35cd
madmarshma11ow <sashagornostaev35@gmail.com>
2026-04-10 18:25:34 +0300

    init

--- index4.js
+++
@@ -1,9 +0,0 @@
-function wait(ms) {
-  return new Promise((res) => setTimeout(() => res(), ms));
-}
-
-async function code() {
-  await wait(1000);
-  console.log('success'); // Выведется через 1000 мс
-}
-code();
